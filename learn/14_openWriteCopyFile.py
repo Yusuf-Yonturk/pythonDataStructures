ce708a08061cd014bc1de11f267093ef3657bf1d
Yusuf Yonturk <yusufyonturk.kisisel@gmail.com>
2025-10-15 15:33:49 +0300

    14_openWriteCopyFile

---
+++ learn/14_openWriteCopyFile.py
@@ -0,0 +1,14 @@
+with open("./src/example1.txt","r") as philoLine:
+    sequence=philoLine.read()
+    philoLine.seek(0)
+    lines=philoLine.readlines(40)
+    print(sequence)
+    print(lines)
+
+# with open("./src/example1.txt","w") as philoDoc:
+#     philoDoc.write("\nYusuf")
+#     philoDoc.write("Yonturk")
+with open("./src/source.txt","r") as source_file:
+    with open("./src/destination.txt","w") as destination_file:
+        for line in source_file:
+            destination_file.write(line)
